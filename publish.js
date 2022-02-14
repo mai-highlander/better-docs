9d4876d4308a70d5158d614002820b1213ba6771
mai-highlander <76951417+mai-highlander@users.noreply.github.com>
2022-02-14 18:17:52 +0200

    Update publish.js

--- publish.js
+++ publish.js
@@ -403,7 +403,7 @@ function buildGroupNav (members, title) {
   nav += buildMemberNav(members.tutorials || [], 'Tutorials', seenTutorials, linktoTutorial)
   nav += buildMemberNav(members.modules || [], 'Modules', {}, linkto)
   nav += buildMemberNav(members.externals || [], 'Externals', seen, linktoExternal)
-  nav += buildMemberNav(members.namespaces || [], 'Entities', seen, linkto)
+  nav += buildMemberNav(members.namespaces || [], 'API', seen, linkto)
   nav += buildMemberNav(members.classes || [], 'Classes', seen, linkto)
   nav += buildMemberNav(members.interfaces || [], 'Interfaces', seen, linkto)
   nav += buildMemberNav(members.events || [], 'Events', seen, linkto)
